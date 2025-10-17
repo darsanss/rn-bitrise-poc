f2fce9ad908ad048608b2045159e675c43b5964e
Darsan S <darsan.s@armiasystems.com>
2025-10-17 23:16:07 +0530

    Template Reset

--- App.tsx
+++ App.tsx
@@ -1,12 +1,12 @@
 import React from 'react';
 import { StatusBar } from 'react-native';
-import TemplateTester from './components/TemplateTester';
+import TemplateRouter from './components/TemplateRouter';
 
 const App = () => {
   return (
     <>
       <StatusBar barStyle="light-content" backgroundColor="#000" />
-      <TemplateTester />
+      <TemplateRouter />
     </>
   );
 };
